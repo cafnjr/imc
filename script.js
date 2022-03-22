4a04f286e46362fa0e12e31d61f65d6f3c970412
Carlos Junior <cafnjr@gmail.com>
2022-03-22 18:49:26 -0300

    imc

---
+++ script.js
@@ -0,0 +1,47 @@
+var peso;
+var altura;
+var imc;
+var resultado;
+
+
+function calcular(){
+    peso = document.getElementById('peso').value;
+    altura = document.getElementById('altura').value;
+    imc = peso/(altura*altura);
+    if( peso == ''){
+        alert('Digite seu peso')
+        return false;
+    }
+     else if( altura == ''){
+        alert('Digite sua Altura')
+        return false;
+
+    }else if(imc < 17){
+        resultado = document.getElementById("resultado");
+        resultado.innerHTML = '<br/> Seu IMC e:<br/> '+ imc.toFixed(2) + "<br/><h3>Você esta muito abaixo do peso </h3>";
+        document.getElementById("altura").value = "";
+        document.getElementById("peso").value = "";
+        return false;
+    }else if(imc > 17 && imc < 18.49){
+        resultado = document.getElementById("resultado");
+        resultado.innerHTML = '<br/> Seu IMC e:<br/> '+ imc.toFixed(2) + "<br/><h3>Você esta abaixo peso ideal</h3>";
+        document.getElementById("altura").value = "";
+        document.getElementById("peso").value = "";
+        return false;
+    }else if(imc > 18.49 && imc < 24.99){
+        resultado = document.getElementById("resultado");
+        resultado.innerHTML = '<br/> Seu IMC e:<br/> '+ imc.toFixed(2) + "<br/><h3>Você esta peso ideal</h3>";
+        document.getElementById("altura").value = "";
+        document.getElementById("peso").value = "";
+        return false;
+    }else if(imc > 24.99){
+    resultado = document.getElementById("resultado");
+    resultado.innerHTML = '<br/> Seu IMC e:<br/> '+ imc.toFixed(2) + "<br/><h3>Você esta acima do peso </h3>";
+    document.getElementById("altura").value = "";
+    document.getElementById("peso").value = "";
+    return false;
+}
+
+
+    
+}
